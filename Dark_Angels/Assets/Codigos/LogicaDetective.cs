13d1488904d2782f6d997dec0b407658bd9c5659
DiegoAndresB <69695050+DiegoAndresB@users.noreply.github.com>
2020-09-19 23:19:39 -0500

    Mecanicas basicas

--- Dark_Angels/Assets/Codigos/LogicaDetective.cs
+++ Dark_Angels/Assets/Codigos/LogicaDetective.cs
@@ -6,13 +6,53 @@ public class LogicaDetective : MonoBehaviour
 {
     public float velocidadMovimiento = 5.0f;
     public float velocidadRotacion = 200.0f;
+
+    //cambio de collider:
+    public CapsuleCollider colParado;
+    public CapsuleCollider colAgachado;
+    public GameObject cabeza;
+    public LogicaCabeza logicaCabeza;
+    public bool estoyAgachado;
+    
+
     private Animator anim;
     public float x, y;
+
+    public Rigidbody rb;
+    public float fuerzaSalto = 8f;
+    public bool puedoSaltar;
+
+    public float velocidadInicial;
+    public float velocidadAgachado;
+
+    public bool estoyAtacando;
+    public bool avanzoSolo;
+    public float impulsoGolpe = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
+        puedoSaltar = false;
         anim = GetComponent<Animator>();
+
+        velocidadInicial = velocidadMovimiento;
+        velocidadAgachado = velocidadMovimiento * 0.5f;
     }
+
+    void FixedUpdate()
+    {
+        if(!estoyAtacando)
+        {
+            transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+            transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+        }
+
+        if(avanzoSolo)
+        {
+            rb.velocity = transform.forward * impulsoGolpe;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -20,10 +60,80 @@ public class LogicaDetective : MonoBehaviour
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
 
-        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
-        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+        if (Input.GetKeyDown(KeyCode.Return) && puedoSaltar && !estoyAtacando)
+        {
+            anim.SetTrigger("golpeo");
+            estoyAtacando = true;
+        } 
 
         anim.SetFloat("VelX",x);
         anim.SetFloat("VelY",y);
+
+        if(puedoSaltar)
+        {
+            if (!estoyAtacando)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    anim.SetBool("salte", true);
+                    rb.AddForce(new Vector3(0, fuerzaSalto, 0), ForceMode.Impulse);
+                }
+
+                if (Input.GetKey(KeyCode.LeftControl))
+                {
+                    anim.SetBool("agachado", true);
+                    velocidadMovimiento = velocidadAgachado;
+
+                    //cambio de collider:
+                    colAgachado.enabled = true;
+                    colParado.enabled = false;
+
+                    cabeza.SetActive(true);
+                    estoyAgachado = true;
+                }
+
+                else
+                {
+                    if(logicaCabeza.contadorColision <= 0)
+                    {
+                        anim.SetBool("agachado", false);
+                        velocidadMovimiento = velocidadInicial;
+
+                        //cambio de collider:
+                        cabeza.SetActive(false);
+                        colAgachado.enabled = false;
+                        colParado.enabled = true;
+                        estoyAgachado = false;
+                    }           
+                }
+            }
+          
+            anim.SetBool("tocoSuelo", true);
+        }
+        else
+        {
+            EstoyCayendo();
+        }
+    }
+
+    public void EstoyCayendo()
+    {
+        anim.SetBool("tocoSuelo", false);
+        anim.SetBool("salte", false);
+    }
+
+    public void DejeDeGolpear()
+    {
+        estoyAtacando = false;
+    }
+
+    public void AvanzoSolo()
+    {
+        avanzoSolo = true;
+    }
+
+    public void DejoDeAvanzar()
+    {
+        avanzoSolo = false;
     }
 }
